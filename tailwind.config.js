261dc5232ca67f37d53d70a3309b9b67ea28caef
solisoma <solisomamadubueze@gmail.com>
2024-03-03 08:45:26 +0100

    Ui(init)

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
+  theme: {
+    screens: { md: "960px" },
+    extend: {},
+  },
+  plugins: [],
+};
