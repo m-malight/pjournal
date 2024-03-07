502426e2c7cfc9cd8c1092017abab70b51cfc49b
solisoma <solisomamadubueze@gmail.com>
2024-03-07 04:06:48 +0100

    fix-reactivity error

--- src/lib/ProductDetails.svelte
+++ src/lib/ProductDetails.svelte
@@ -1,6 +1,10 @@
 <script>
     import IoIosCash from 'svelte-icons/io/IoIosCash.svelte'
     export let details;
+    $: name = details.name;
+    $: date = details.date;
+    $: amount = details.amount;
+    $: desc = details.desc;
 </script>
 
 <div>
@@ -8,10 +12,10 @@
         <IoIosCash />
     </div>
     <div class="flex justify-between">
-        <h2 class="font-bold text-xm md:text-xl">{details.name}</h2>
-        <h2 class="font-normal text-xm text-gray-500 md:text-xl">{details.date}</h2>
+        <h2 class="font-bold text-xm md:text-xl">{name}</h2>
+        <h2 class="font-normal text-xm text-gray-500 md:text-xl">{date}</h2>
     </div>
-    <h2 class="font-normal text-xm text-gray-500 md:text-xl">${details.amount}</h2>
+    <h2 class="font-normal text-xm text-gray-500 md:text-xl">${amount}</h2>
     <h2 class="text-black mt-3 text-xl font-bold">Description</h2>
-    <p class="font-normal text-xm text-gray-500 md:text-xl">{details.desc}</p>
+    <p class="font-normal text-xm text-gray-500 md:text-xl">{desc}</p>
 </div>
